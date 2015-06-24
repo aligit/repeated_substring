0841c80629285d88c84622e7a606e20b01821427
aivic-unigedesk <ali.ivic88@gmail.com>
2015-06-24 11:59:15 +0200

    Writes test-case that expects find_repeating_str method to be implemented with a string argument

--- spec/repeated_substring_spec.rb
+++ spec/repeated_substring_spec.rb
@@ -5,4 +5,7 @@ RSpec.describe 'repeated_substring' do
   it 'is an example' do
     expect(RepeatedSubstring.example_method).to eq(3)
   end
+  it 'has an argument' do
+    expect(RepeatedSubstring).to receive(:find_repeating_strs).with("banana\nam so uniqe").at_least(:once)
+  end
 end
